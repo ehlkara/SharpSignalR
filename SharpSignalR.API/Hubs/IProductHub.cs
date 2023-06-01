c8edd48bb9821eb5c0f5cc142d63583f9684dbb4
ehlkara <ehlullah.fb@gmail.com>
2023-06-01 16:16:56 +0300

    created signalR server strongly typed hubs

---
+++ SharpSignalR.API/Hubs/IProductHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SharpSignalR.API.Models;
+
+namespace SharpSignalR.API.Hubs
+{
+	public interface IProductHub
+	{
+		Task ReceiveProduct(Product p);
+	}
+}
+
---
+++ SharpSignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using SharpSignalR.API.Models;
+
+namespace SharpSignalR.API.Hubs
+{
+	public class ProductHub : Hub<IProductHub>
+	{
+		public async Task SendProduct(Product p)
+		{
+			await Clients.All.ReceiveProduct(p);
+		}
+	}
+}
+
