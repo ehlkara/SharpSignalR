6196ffdd22799207fad770c556f04cfa19b9cdc3
ehlkara <ehlullah.fb@gmail.com>
2023-06-04 15:48:58 +0300

    created covidapi project and migrations

---
+++ SharpCovidChart.API/Models/Covid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SharpCovidChart.API.Models
+{
+	public enum ECity
+	{
+		Istanbul = 1,
+		Ankara,
+		Izmir,
+		Konya,
+		Sivas,
+		Antalya
+	}
+
+
+	public class Covid
+	{
+		public int Id { get; set; }
+		public ECity City { get; set; }
+		public int Count { get; set; }
+		public DateTime CovidDate { get; set; }
+	}
+}
+
---
+++ SharpCovidChart.API/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using SharpCovidChart.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("ConStr"), options =>
+    {
+        options.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    }).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
